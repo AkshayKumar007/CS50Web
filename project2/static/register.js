21723b58a7c296805925902b327bbb434bd6059d
AkshayKumar007 <akshaykrcc@live.com>
2019-08-10 21:41:14 +0530

    debugged HTML's and JS files

--- project2/static/register.js
+++ project2/static/register.js
@@ -1,41 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     document.querySelector('#form').onsubmit = () => {
+
         const request = new XMLHttpRequest();
+
         const fname = document.querySelector('#fname').value;//change for variables in DOM
         const dname = document.querySelector('#dname').value;
         const email = document.querySelector('#email').value;
         const passwd = document.querySelector('#passwd').value;
+        
         request.open('POST', '/register');
         request.onload = () => {
-
-            // Extract JSON data from request
             const data = JSON.parse(request.responseText);
 
             if (data.message == "no_mail") {// check for white-spacing of curly braces
-                const contents = `Error! Looks like <a href="{{ url_for('index') }}" class="alert-link">email</a> is already taken.`
+                const contents = '<div class="alert alert-primary" role="alert">Error! Looks like email is already taken."</div>';
                 document.querySelector('#message').innerHTML = contents;
             }
             else if (data.message == "no_dname"){
-                const contents = `Error! Looks like <a href="{{ url_for('index') }}" class="alert-link">Display Name</a> is already taken.`
+                const contents = '<div class="alert alert-primary" role="alert">Error! Looks like Display Name is already taken."</div>';
                 document.querySelector('#result > .alert').innerHTML = contents;
             }
         }
         const data = new FormData();
-        data.append('fname', fname); //change for variables you created
+        data.append('fname', fname); 
         data.append('dname', dname);
         data.append('email', email);
         data.append('passwd', passwd);
         // Send request
         request.send(data);
-        return false;
+        return true;
     }
 });
 
-<div class="alert alert-primary" role="alert">
-                    <!--error message will appear here-->
-                    <!-- Error! Looks like <a href="{{ url_for('index') }}" class="alert-link">Display Name</a> is already taken. -->
-                </div>
 
 // function message(vars) {
     
